eb92a35cdb84aada9577d5ba9cc7e4a5797bbf1b
Tamara Chance <tamara.n.chance@gmail.com>
2021-11-04 16:26:58 -0500

    register, compare, verify

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,21 +1,35 @@
+const bcryptjs = require('bcryptjs');
 const users = []
 
 module.exports = {
-    login: (req, res) => {
-      console.log('Logging In User')
-      console.log(req.body)
-      const { username, password } = req.body
-      for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
-        }
-      }
-      res.status(400).send("User not found.")
-    },
-    register: (req, res) => {
-        console.log('Registering User')
-        console.log(req.body)
-        users.push(req.body)
-        res.status(200).send(req.body)
+  login: (req, res) => {
+    console.log('Logging In User')
+    console.log(req.body)
+    const { username, password } = req.body
+    let userfound = false;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].username === username){
+        userfound = true;
+        bcrypt.compare(password,users[i].password).then(result => {
+          if(result){
+            console.log(result);
+            res.status(200).send(users[i]);
+          }
+      }).catch(err => console.log(err));
     }
+    if(userfound == false){
+      res.status(400).send("User not found.");
+    }
+  }
+},
+  register: (req, res) => {
+      console.log('Registering User')
+      let salt = bcrypt.genSaltSync(10);
+      let hash = bcrypt.hashSync(req.body.password,salt);
+      let user = req.body;
+      user.password = hash;
+      users.push(user);
+      console.log(users);
+      res.status(200).send(user);
+  }
 }
